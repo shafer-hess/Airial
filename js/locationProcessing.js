b451e4ebb8d94a9ffe77ffdc10afb65562ef9ae0
EC2 Default User <ec2-user@ip-172-31-30-251.us-east-2.compute.internal>
2018-12-04 19:28:24 +0000

    added country request and select elements for state and city

--- js/locationProcessing.js
+++ js/locationProcessing.js
@@ -1,21 +1,45 @@
 const key = 'SunWZiorHPdTniAk2';
 
-var settings = {
+var countrySettings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "api.airvisual.com/v2/countries?key=" + key,
+	"url": "http://api.airvisual.com/v2/countries?key=" + key,
 	"method": "GET",
 	"headers": {}
 };
 
 $(document).ready(function() {
 	//Get valid Countries
-	$.ajax(settings).done(function(response)) {
+	$.ajax(countrySettings).done(function(response) {
+		
 		var countryList = "";
-		for(country in response.data) {
-			console.log(country);	
+		for(let country of response.data) {
+			let val = "<option value=" + country.country + ">" + country.country + "</option>";
+			countryList += val;	
 		}
-	}
+		
+		$("#countries").append(countryList);
+				
+
+	});
+
+	//State Request
+	$("#countries").on("change", function() {
+		var selected = $("#countries").val(); 
+		if(selected != "") {
+			console.log(selected);
+		}
+	});
+	
+
+	//City Request
+
+
+
+
+
+
+
 });
 
 
