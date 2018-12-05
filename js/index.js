e50af1652fc0ca34c3b28ff51f4097136b090cbb
EC2 Default User <ec2-user@ip-172-31-30-251.us-east-2.compute.internal>
2018-12-05 22:23:24 +0000

    added color change code to index.js

--- js/index.js
+++ js/index.js
@@ -11,6 +11,33 @@ var settings = {
         "headers": {}
 }
 
+var colors = {
+	"green": "rgb(108,223,67)",
+	"yellow": "rgb(255,254,85)",
+	"orange": "rgb(236,134,51)",
+	"red": "rgb(231,51,35)",
+	"purple": "rgb(138,27,75)",
+	"maroon": "rgb(114,20,39)"
+}
+
+var range = {
+	"green": {"min": 0, "max": 50},
+	"yellow": {"min": 51, "max": 100},
+	"orange": {"min": 101, "max": 150},
+	"red": {"min": 151, "max": 200},
+	"purple": {"min": 201, "max": 300},
+	"maroon": {"min": 301, "max": 500}
+}
+
+function getColor(aqi) {
+	if(aqi >= range.green.min && aqi <= range.green.max) { return colors.green; }
+	else if(aqi >= range.yellow.min && aqi <= range.yellow.max) { return colors.yellow; }
+	else if(aqi >= range.orange.min && aqi <= range.orange.max) { return colors.orange; }
+	else if(aqi >= range.red.min && aqi <= range.red.max) { return colors.red; }
+	else if(aqi >= range.purple.min && aqi <= range.purple.max) { return colors.purple; }
+	else { return colors.maroon; }
+}
+
 $.ajax(settings).done(function (response) {
         console.log(response);
 
@@ -35,6 +62,7 @@ $.ajax(settings).done(function (response) {
 	var humidity = document.getElementById("humidity");
 	var pressure = document.getElementById("pressure");
 	var wspeed = document.getElementById("speed");
+	var color = getColor(response.data.current.pollution.aqius);
 
 	loc.innerHTML = response.data.city;
 	winfo.innerHTML = weather.image;
@@ -43,7 +71,7 @@ $.ajax(settings).done(function (response) {
 	pressure.innerHTML = "Pressure:<br>" + weather.pressure + "hPa";
 	wspeed.innerHTML = "Wind Speed:<br>" + weather.wspeed + "mph";
 
-
+	$("#aqi-color").css("background-color", color);
 
 	city.innerHTML = weather.image + "<p>" + response.data.city + "<p>";
 });
